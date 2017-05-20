1a538d68527ccb9dac8c694cbbba620a3501f429
Jose Manuel <josemalvarezg1@gmail.com>
2017-05-20 12:32:37 -0400

    Nueva interfaz de fechas en Reportes.

--- static/js/reportes.js
+++ static/js/reportes.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$( "#boton_generar" ).click(function() {
 
 		valor_select = $('#tipo').val();
-  		console.log(valor_select);
+  		//console.log(valor_select);
 
   		select_fecha = $('#select_fecha').val();
 
@@ -16,12 +16,15 @@ $(document).ready(function(){
   				if ($('#fecha1').val() == "" || $('#fecha2').val() == "") {
 			      alert("Los campos de fecha son requeridos");
 			    } else {
-	  				var desde = new Date( $('#fecha1').val() );
-					var hasta = new Date( $('#fecha2').val() );
+			    	var datearray = $('#fecha1').val().split("/");
+	  				var newdate = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
+					var desde = new Date(newdate);
+					datearray = $('#fecha2').val().split("/");
+					newdate = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
+					var hasta = new Date(newdate);
 
 					desde = desde.getFullYear().toString() + '/' + (desde.getMonth()+1).toString() + '/' + desde.getDate().toString() ;
 					hasta = hasta.getFullYear().toString() + '/' + (hasta.getMonth()+1).toString() + '/' + hasta.getDate().toString() ;
-
 
 			  		$("#desde_real").val(desde);
 		  			$("#hasta_real").val(hasta);
@@ -35,7 +38,9 @@ $(document).ready(function(){
   				if ($('#fecha1').val() == "") {
   					alert("El campo de fecha es requerido");
   				} else {
-	  				var fecha = new Date( $('#fecha1').val() );
+	  				var datearray = $('#fecha1').val().split("/");
+	  				var newdate = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
+					var fecha = new Date(newdate);
 
 	  				fecha = fecha.getFullYear().toString() + '/' + (fecha.getMonth()+1).toString() + '/' + fecha.getDate().toString() ;
 	  				$("#desde_real").val(fecha);
@@ -46,52 +51,48 @@ $(document).ready(function(){
 
   			} else if (select_fecha == 'Mes') {
 
-  				if ($('#fecha3').val() == "") {
-  					alert("El campo de fecha es requerido");
-  				} else {
-	  				var fecha = $('#fecha3').val();
+  				var mes = $('#select_mes').val();
+  				var anno = $('#select_anno2 :selected').text();
 
-	  				desde = fecha + '/01';
-	  				hasta = fecha + '/31';
+  				desde = anno + '/' + mes + '/01';
+  				hasta = anno + '/' + mes + '/31';
 
-	  				// console.log(desde + ' ' + hasta);
+  				//console.log(desde + ' ' + hasta);
 
-	  				$("#desde_real").val(desde);
-	  				$("#hasta_real").val(hasta);
-	  				$("#solicitud_real").val(valor_select);
-	  				$("#formulario_real").submit();
-
-  				}
+  				$("#desde_real").val(desde);
+  				$("#hasta_real").val(hasta);
+  				$("#solicitud_real").val(valor_select);
+  				$("#formulario_real").submit();
 
   			} else if (select_fecha == 'Año') {
+  				
+  				var anno = $('#select_anno :selected').text();
 
-  				if ($('#fecha4').val() == "") {
-  					alert("El campo de fecha es requerido");
-  				} else {
-	  				var fecha = $('#fecha4').val();
+  				desde = anno + '/01/01';
+  				hasta = anno + '/12/31';
 
-	  				desde = fecha + '/01/01';
-	  				hasta = fecha + '/12/31';
+  				//console.log(desde + ' ' + hasta);
 
-	  				// console.log(desde + ' ' + hasta);
-
-	  				$("#desde_real").val(desde);
-	  				$("#hasta_real").val(hasta);
-	  				$("#solicitud_real").val(valor_select);
-	  				$("#formulario_real").submit();
-
-  				}
+  				$("#desde_real").val(desde);
+  				$("#hasta_real").val(hasta);
+  				$("#solicitud_real").val(valor_select);
+  				$("#formulario_real").submit();
 
   			}
 
-  		} 
+  		} else {
+
+  			$("#solicitud_real").val(valor_select);
+  			$("#formulario_real").submit();
+
+  		}
   		
 	});
 
 	$( "#boton_generar_aux" ).click(function() {
 
 		valor_select = $('#tipo').val();
-  		console.log(valor_select);
+  		//console.log(valor_select);
 
   		select_fecha = $('#select_fecha').val();
 
@@ -100,53 +101,81 @@ $(document).ready(function(){
   			$("#tipo_fecha").val(select_fecha);
 
   			if (select_fecha == 'Intervalo de Tiempo') {
-  				var desde = new Date( $('#fecha1').val() );
-				var hasta = new Date( $('#fecha2').val() );
 
-				desde = desde.getFullYear().toString() + '/' + (desde.getMonth()+1).toString() + '/' + desde.getDate().toString() ;
-				hasta = hasta.getFullYear().toString() + '/' + (hasta.getMonth()+1).toString() + '/' + hasta.getDate().toString() ;
+  				if ($('#fecha1').val() == "" || $('#fecha2').val() == "") {
+			      alert("Los campos de fecha son requeridos");
+			    } else {
+			    	var datearray = $('#fecha1').val().split("/");
+	  				var newdate = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
+					var desde = new Date(newdate);
+					datearray = $('#fecha2').val().split("/");
+					newdate = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
+					var hasta = new Date(newdate);
 
+					desde = desde.getFullYear().toString() + '/' + (desde.getMonth()+1).toString() + '/' + desde.getDate().toString() ;
+					hasta = hasta.getFullYear().toString() + '/' + (hasta.getMonth()+1).toString() + '/' + hasta.getDate().toString() ;
 
-		  		$("#desde_real").val(desde);
-	  			$("#hasta_real").val(hasta);
+			  		$("#desde_real").val(desde);
+		  			$("#hasta_real").val(hasta);
+		  			$("#solicitud_real").val(valor_select);
+  					$("#formulario_real").submit();
+
+		  		}
 
   			} else if (select_fecha == 'Día') {
-  				var fecha = new Date( $('#fecha1').val() );
 
-  				fecha = fecha.getFullYear().toString() + '/' + (fecha.getMonth()+1).toString() + '/' + fecha.getDate().toString() ;
-  				$("#desde_real").val(fecha);
+  				if ($('#fecha1').val() == "") {
+  					alert("El campo de fecha es requerido");
+  				} else {
+	  				var datearray = $('#fecha1').val().split("/");
+	  				var newdate = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
+					var fecha = new Date(newdate);
+
+	  				fecha = fecha.getFullYear().toString() + '/' + (fecha.getMonth()+1).toString() + '/' + fecha.getDate().toString() ;
+	  				$("#desde_real").val(fecha);
+	  				$("#solicitud_real").val(valor_select);
+	  				$("#formulario_real").submit();
+
+	  			}
 
   			} else if (select_fecha == 'Mes') {
 
-  				var fecha = $('#fecha3').val();
+  				var mes = $('#select_mes').val();
+  				var anno = $('#select_anno2 :selected').text();
 
-  				desde = fecha + '/01';
-  				hasta = fecha + '/31';
+  				desde = anno + '/' + mes + '/01';
+  				hasta = anno + '/' + mes + '/31';
 
-  				// console.log(desde + ' ' + hasta);
+  				//console.log(desde + ' ' + hasta);
 
   				$("#desde_real").val(desde);
   				$("#hasta_real").val(hasta);
+  				$("#solicitud_real").val(valor_select);
+  				$("#formulario_real").submit();
 
   			} else if (select_fecha == 'Año') {
+  				
+  				var anno = $('#select_anno :selected').text();
 
-  				var fecha = $('#fecha4').val();
+  				desde = anno + '/01/01';
+  				hasta = anno + '/12/31';
 
-  				desde = fecha + '/01/01';
-  				hasta = fecha + '/12/31';
-
-  				// console.log(desde + ' ' + hasta);
+  				//console.log(desde + ' ' + hasta);
 
   				$("#desde_real").val(desde);
   				$("#hasta_real").val(hasta);
+  				$("#solicitud_real").val(valor_select);
+  				$("#formulario_real").submit();
 
   			}
 
-  		} 
+  		} else {
 
-  		$("#solicitud_real").val(valor_select);
-  		$("#formulario_real").submit();
+  			$("#solicitud_real").val(valor_select);
+  			$("#formulario_real").submit();
 
+  		}
+  		
 	});
 
 
@@ -161,17 +190,95 @@ $(document).ready(function(){
 			$('#span2').hide();
 			$('#date1').hide();
 			$('#date2').hide();
+			$('#date3').hide();
+			$('#date4').hide();
+			$('#date5').hide();
 			$('#rango_fechas').hide();	
 			$('#select_fecha').hide();
 			$('#boton_generar').hide();
 			$('#boton_generar_aux').show();
 		} else {
-			$('#fecha1').show();
-			$('#fecha2').show();
-			$('#span1').show();
-			$('#span2').show();
-			$('#date1').show();
-			$('#date2').show();
+			valor_select = $('#select_fecha').val();
+
+			if (valor_select == "Día") {
+				$('#fecha1').show();
+				$('#span1').show();
+				$('#date1').show();
+
+				$('#fecha2').hide();
+				$('#span2').hide();
+				$('#date2').hide();
+
+				$('#fecha3').hide();
+				$('#span3').hide();
+				$('#date3').hide();
+
+				$('#fecha4').hide();
+				$('#span4').hide();
+				$('#date4').hide();
+
+				$('#date5').hide();
+
+			}else if (valor_select == "Intervalo de Tiempo") {
+				$('#fecha1').show();
+				$('#span1').show();
+				$('#date1').show();
+
+				$('#fecha2').show();
+				$('#span2').show();
+				$('#date2').show();
+
+				$('#fecha3').hide();
+				$('#span3').hide();
+				$('#date3').hide();
+
+				$('#fecha4').hide();
+				$('#span4').hide();
+				$('#date4').hide();
+
+				$('#date5').hide();
+
+				
+			} else if (valor_select == "Mes") {
+
+				$('#fecha3').show();
+				$('#span3').show();
+				$('#date3').show();
+
+				$('#fecha1').hide();
+				$('#span1').hide();
+				$('#date1').hide();
+
+				$('#fecha2').hide();
+				$('#span2').hide();
+				$('#date2').hide();
+
+				$('#fecha4').show();
+				$('#span4').hide();
+				$('#date4').hide();
+
+				$('#date5').show();
+
+			} else {
+				$('#fecha3').hide();
+				$('#span3').hide();
+				$('#date3').hide();
+
+				$('#fecha1').hide();
+				$('#span1').hide();
+				$('#date1').hide();
+
+				$('#fecha2').hide();
+				$('#span2').hide();
+				$('#date2').hide();
+
+				$('#fecha4').show();
+				$('#span4').show();
+				$('#date4').show();
+
+				$('#date5').hide();
+
+			}
 			$('#rango_fechas').show();			
 			$('#select_fecha').show();
 			$('#boton_generar').show();
@@ -201,6 +308,8 @@ $(document).ready(function(){
 			$('#span4').hide();
 			$('#date4').hide();
 
+			$('#date5').hide();
+
 		}else if (valor_select == "Intervalo de Tiempo") {
 			$('#fecha1').show();
 			$('#span1').show();
@@ -218,6 +327,8 @@ $(document).ready(function(){
 			$('#span4').hide();
 			$('#date4').hide();
 
+			$('#date5').hide();
+
 			
 		} else if (valor_select == "Mes") {
 
@@ -233,9 +344,11 @@ $(document).ready(function(){
 			$('#span2').hide();
 			$('#date2').hide();
 
-			$('#fecha4').hide();
+			$('#fecha4').show();
 			$('#span4').hide();
 			$('#date4').hide();
+
+			$('#date5').show();
 
 		} else {
 			$('#fecha3').hide();
@@ -254,13 +367,9 @@ $(document).ready(function(){
 			$('#span4').show();
 			$('#date4').show();
 
+			$('#date5').hide();
 		}
 
 	})
-
-
-
-
-
 
 }); //Fin de document ready
